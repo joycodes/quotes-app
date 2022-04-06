f8893b4ed2170ba53d01391d7a61f2cfbd82f750
Joyce Nguttu <jnguttu@gmail.com>
2022-04-06 04:25:40 +0300

    add all files

--- Goals/goals/src/app/app.component.ts
+++ Goals/goals/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { Goal } from './goal';
+//....
 export class AppComponent {
-  title = 'goals';
+  
 }
+
