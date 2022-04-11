9808ed3a76995f9de5376bd0a5840d50e8a366a4
Joyce Nguttu <jnguttu@gmail.com>
2022-04-11 16:33:00 +0300

    remove unnecesary files

--- quotes-app/src/app/time-quote.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeQuotePipe } from './time-quote.pipe';
-
-describe('TimeQuotePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeQuotePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
