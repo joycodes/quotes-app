105f6413585601a1430fef053c0ac73f84eda745
Joyce Nguttu <jnguttu@gmail.com>
2022-04-11 05:06:24 +0300

    add function that allows adding of a new quote

--- Quotes/src/app/quotes-form/quotes-form.component.ts
+++ Quotes/src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  newQuotes = new Quotes ("","","",new Date(),0,0);
+    @Output() addQuotes = new EventEmitter < Quotes > ();
+
+    // function that creates a new quote array
+
+    submittingQuote(){
+
+        this.addQuotes.emit(this.newQuotes);
+        this.newQuotes = new Quotes("","","",new Date(),0,0)
+    }
 
   constructor() { }
 
